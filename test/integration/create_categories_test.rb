6c82e27c47389ec33b994562a68744d3effaedd9
Liviu Cauia <liviu.cauia@gmail.com>
2020-02-03 15:44:27 +0200

    admin test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,15 +1,35 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-  test "invalid category submission results in failure" do
-    get new_category_path
-    assert_template 'categories/new'
-    assert_no_difference 'Category.count' do
-      post categories_path, params: { category: {name: " "} }
-    end
+  require 'test_helper'
 
-    assert_template 'categories/new'
-    assert_select 'h2.panel-title'
-    assert_select 'div.panel-body'
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "Sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
+
+  test "should get categories index" do
+    get categories_path
+    assert_response :success
+  end
+
+  test "should get new" do
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    get category_path(@category)
+    assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "sports" } }
+    end
+    assert_redirected_to categories_path
+  end
+end
 end
